a147d1cdd4632c6201a9582773a9d237bbfdd7f1
James Liu <87109954+liusj5257@users.noreply.github.com>
2023-04-27 10:09:40 +0800

    send

---
+++ send.py
@@ -0,0 +1,10 @@
+import os
+
+from onepush import notify
+
+PUSH_PLUS_TOKEN = os.environ['PUSH_PLUS_TOKEN']
+
+
+with open('output.log', 'r', encoding='utf-8') as f:
+    log_content = f.read()
+notify('pushplus', token='${{ secrets.PUSH_PLUS_TOKEN }}', title='星铁签到', content=log_content)
