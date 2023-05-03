b7c0c02a54c1b54334ca28824acd761e9fff34e1
James Liu <87109954+liusj5257@users.noreply.github.com>
2023-05-03 09:28:25 +0800

    Update send.py

--- send.py
+++ send.py
@@ -7,5 +7,6 @@ PUSH_PLUS_TOKEN = os.environ['PUSH_PLUS_TOKEN']
 
 with open('output.log', 'r', encoding='utf-8') as f:
     log_content = f.read()
-if "Message: OK" in log_content:
-    notify('pushplus', token=PUSH_PLUS_TOKEN, title='星铁签到', content=log_content)
+if "Message: OK" not in log_content:
+    notify('pushplus', token=PUSH_PLUS_TOKEN, title='星铁签到_Warning', content=log_content)
+
