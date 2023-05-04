721a9c1139b135d6a1d8edb17f1dde9f259b4fbd
James Liu <87109954+liusj5257@users.noreply.github.com>
2023-05-04 16:03:07 +0800

    test wx app

--- send.py
+++ send.py
@@ -5,10 +5,18 @@ print(os.environ)
 from onepush import notify
 
 PUSH_PLUS_TOKEN = os.environ['PUSH_PLUS_TOKEN']
+AGENTID = os.environ['AGENTID']
+CORPID = os.environ['CORPID']
+CORPSECRET = os.environ['CORPSECRET']
+PROVIDER = os.environ['PROVIDER']
 
 
 with open('output.log', 'r', encoding='utf-8') as f:
     log_content = f.read()
-if "Message: OK" not in log_content:
-    notify('pushplus', token=PUSH_PLUS_TOKEN, title='星铁签到_Warning', content=log_content)
 
+
+if "Message: OK" not in log_content:
+    if PROVIDER == 'pushplus':
+        notify('pushplus', token=PUSH_PLUS_TOKEN, title='星铁签到_Warning', content=log_content)
+    elif PROVIDER == '1':
+        notify('wechatworkapp',corpid=CORPID,corpsecret=CORPSECRET,agentid=AGENTID,title='星铁签到',content=log_content)
