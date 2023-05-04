169056ffb026ee4ad79c4f09775c5b5431bbf21a
James Liu <87109954+liusj5257@users.noreply.github.com>
2023-05-04 18:31:53 +0800

    TEST

--- send.py
+++ send.py
@@ -9,18 +9,23 @@ AGENTID = os.environ['AGENTID']
 CORPID = os.environ['CORPID']
 CORPSECRET = os.environ['CORPSECRET']
 PROVIDER = os.environ['PROVIDER']
-
-
+WEBHOOK = os.environ['WEBHOOK']
+CHANNEL = os.environ['CHANNEL']
 with open('output.log', 'r', encoding='utf-8') as f:
     log_content = f.read()
 
 
 if "Message: OK" not in log_content:
-    # if PROVIDER == 'pushplus':
-    #     notify('pushplus', token=PUSH_PLUS_TOKEN, title='星铁签到_Warning', content=log_content)
-    # elif PROVIDER == '1':
-    n = get_notifier('wechatworkapp')
-    print(n.params)    
-    response = n.notify(corpid=CORPID,corpsecret=CORPSECRET,agentid=AGENTID,title='星铁签到',content=log_content)
-    print(response.text)    
+    n = get_notifier(PROVIDER)
+    print(n.params)   
+    if PROVIDER == 'pushplus':
+        response = n.notify(token=PUSH_PLUS_TOKEN,title='星铁签到',content=log_content,webhook=WEBHOOK,channel = CHANNEL)
+        print(response.text)    
 
+
+
+
+    # n = get_notifier(PROVIDER)
+    # print(n.params)    
+    # response = n.notify(corpid=CORPID,corpsecret=CORPSECRET,agentid=AGENTID,title='星铁签到',content=log_content)
+    # print(response.text)
